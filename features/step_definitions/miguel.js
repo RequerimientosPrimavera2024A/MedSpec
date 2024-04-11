8bcc2c74b68e0fe3331b8ed8552f163e3d3c1248
Maigol <migueltorrespvr2@gmail.com>
2024-04-11 08:27:42 -0600

    desde mi branch

---
+++ features/step_definitions/miguel.js
@@ -0,0 +1,126 @@
+const assert = require('assert');
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+Given('que un usuario ha iniciado sesión en la aplicación MedSpec', function () {
+    return 'pending';
+});
+
+Given('que un usuario ha realizado una búsqueda y ha encontrado un servicio de interés', function () {
+    return 'pending';
+});
+
+When('el usuario selecciona el servicio que desea ver para obtener más información', function () {
+    return 'pending';
+});
+
+Then('se muestra al usuario la información detallada del servicio', function () {
+    return 'pending';
+});
+
+Then('el usuario puede navegar por la información proporcionada o realizar acciones adicionales, como agendar una cita o contactar al servicio', function () {
+    return 'pending';
+});
+
+Then('se garantiza que la información proporcionada sea precisa y actualizada', function () {
+    return 'pending';
+});
+
+Given('que un usuario ha encontrado un servicio de interés y ha accedido a su información detallada', function () {
+    return 'pending';
+});
+
+When('el usuario visualiza la dirección exacta del servicio en la aplicación', function () {
+    return 'pending';
+});
+
+Then('el usuario compara la dirección proporcionada en la aplicación con la ubicación física en la que se encuentra', function () {
+    return 'pending';
+});
+
+Then('el usuario puede utilizar herramientas de navegación externas, como mapas, para verificar la ubicación del servicio en el mundo real', function () {
+    return 'pending';
+});
+
+Then('el usuario puede revisar comentarios y reseñas de otros usuarios sobre la ubicación del servicio para obtener una idea adicional de su autenticidad', function () {
+    return 'pending';
+});
+
+Then('el usuario puede ponerse en contacto directamente con el servicio a través de la aplicación para confirmar la ubicación', function () {
+    return 'pending';
+});
+
+Given('que un usuario ha encontrado un servicio médico de interés y ha accedido a su información detallada', function () {
+    return 'pending';
+});
+
+When('el usuario busca información específica sobre el médico asociado al servicio', function () {
+    return 'pending';
+});
+
+Then('se muestra al usuario la información detallada del médico', function () {
+    return 'pending';
+});
+
+Then('el usuario puede revisar la cédula profesional del médico para verificar su validez y autenticidad', function () {
+    return 'pending';
+});
+
+Then('la información proporcionada debe ser precisa y actualizada', function () {
+    return 'pending';
+});
+
+Then('se garantiza la privacidad y seguridad de la información sensible del médico y del usuario', function () {
+    return 'pending';
+});
+
+Given('que un usuario ha encontrado un servicio médico de interés y han accedido a su información detallada', function () {
+    return 'pending';
+});
+
+When('el usuario desean verificar la precisión y exactitud de la información proporcionada en la aplicación', function () {
+    return 'pending';
+});
+
+Then('los usuarios pueden comparar la información mostrada en la aplicación con otras fuentes de información externas, como sitios web médicos confiables o registros gubernamentales', function () {
+    return 'pending';
+});
+
+Then('los usuarios pueden comunicarse directamente con el servicio médico a través de la aplicación para obtener aclaraciones o confirmaciones adicionales sobre la información proporcionada', function () {
+    return 'pending';
+});
+
+Then('los usuarios pueden revisar reseñas y comentarios de otros usuarios sobre el servicio médico para obtener una perspectiva adicional sobre su calidad y confiabilidad', function () {
+    return 'pending';
+});
+
+Then('la aplicación debe garantizar que la información proporcionada sea precisa, actualizada y verificada por fuentes confiables antes de ser mostrada a los usuarios', function () {
+    return 'pending';
+});
+
+Given('la empresa médica ha iniciado sesión en la aplicación MedSpec con sus credenciales de empresa', function () {
+    return 'pending';
+});
+
+Given('la empresa médica tiene acceso a la sección administrativa de la aplicación', function () {
+    return 'pending';
+});
+
+When('la empresa médica accede a la sección de gestión de certificaciones y leyes de la aplicación', function () {
+    return 'pending';
+});
+
+Then('la empresa médica puede ver y gestionar las certificaciones médicas necesarias para los médicos asociados con su empresa en la aplicación', function () {
+    return 'pending';
+});
+
+Then('la empresa médica puede garantizar que los médicos asociados cumplan con las leyes y regulaciones médicas aplicables en la región en la que operan', function () {
+    return 'pending';
+});
+
+Then('la empresa médica puede cargar y actualizar documentos de certificación, licencias médicas y otros documentos legales relevantes de los médicos asociados en la aplicación', function () {
+    return 'pending';
+});
+
+Then('la empresa médica puede revisar y mantener actualizada la información legal y regulatoria relacionada con la práctica médica en la aplicación', function () {
+    return 'pending';
+});
